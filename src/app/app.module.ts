98279d6cd43c9b78b4bf7982907fbdb7c0773e90
Слава Устинов <slavaustinov@Mac-mini-Slava-2.local>
2021-05-18 23:44:31 +0300

    remove memo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MemoComponent } from './memo/memo.component';
 import { FormsExampleComponent } from './template-forms-example/forms-example.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ReactiveFormExampleComponent } from './reactive-form-example/reactive-form-example.component';
@@ -10,7 +9,6 @@ import { ReactiveFormExampleComponent } from './reactive-form-example/reactive-f
 @NgModule({
   declarations: [
     AppComponent,
-    MemoComponent,
     FormsExampleComponent,
     ReactiveFormExampleComponent,
   ],
